ccd458ab6683f68d8c397a666a4bee5c80101a57
happyu0223 <753556090@qq.com>
2016-01-07 18:50:33 +0800

    check mapping

---
+++ KnockOutDemoMvc/KnockOutDemoMvc/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KnockOutDemoMvc.Controllers
+{
+    public class  Pepople 
+    {
+        public int ID { get; set; }
+        public string   Name { get; set; }
+    }
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View(new List<Pepople>
+            {
+                new Pepople{ID=1,Name="test1"},
+                new Pepople{ID=2,Name="test2"},
+                new Pepople{ID=3,Name="test3"},
+                new Pepople{ID=4,Name="test4"},
+                new Pepople{ID=5,Name="test5"},
+                new Pepople{ID=6,Name="test6"},
+            });
+        }
+
+        public ActionResult MultiSelectList()
+        {
+            return View();
+        }
+    }
+}
