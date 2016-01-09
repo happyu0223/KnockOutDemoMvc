b770ac032076151680bbacf2c99cc6991de708fc
happyu0223 <753556090@qq.com>
2016-01-09 16:32:17 +0800

    check in

--- KnockOutDemoMvc/KnockOutDemoMvc/Controllers/HomeController.cs
+++ KnockOutDemoMvc/KnockOutDemoMvc/Controllers/HomeController.cs
@@ -17,6 +17,26 @@ namespace KnockOutDemoMvc.Controllers
         // GET: /Home/
         public ActionResult GetPepopleList()
         {
+            var data = new List<Pepople>
+            {
+                new Pepople{ID=1,Name="test1"},
+                new Pepople{ID=2,Name="test2"},
+                new Pepople{ID=3,Name="test3"},
+                new Pepople{ID=4,Name="test4"},
+                new Pepople{ID=5,Name="test5"},
+                new Pepople{ID=6,Name="test6"},
+            };
+            //var connectedContract = ManagersProvider.ContractWholeManager.GetViewModelByContractInfoId(id);
+            //return JavaScriptJson(new AjaxReturnInfo { Data = connectedContract }, JsonRequestBehavior.AllowGet);
+
+            //var data = ManagersProvider.LongContractManager.List(paramter, pagination);
+            //var result = new AjaxReturnInfo { Data = new { pagination = pagination, result = data, } };
+            //return Json(result, JsonRequestBehavior.AllowGet);
+            return JavaScriptJson(new AjaxReturnInfo { Status = true, Data = data }, JsonRequestBehavior.AllowGet);
+        }
+
+        public List<Pepople> GetList()
+        {
             var result = new List<Pepople>
             {
                 new Pepople{ID=1,Name="test1"},
@@ -26,10 +46,9 @@ namespace KnockOutDemoMvc.Controllers
                 new Pepople{ID=5,Name="test5"},
                 new Pepople{ID=6,Name="test6"},
             };
-
-            return JavaScriptJson(new AjaxReturnInfo { Status = true, Data = result }, JsonRequestBehavior.AllowGet);
+            return result;
+            //  return JavaScriptJson(new AjaxReturnInfo { Status = true, Data = result }, JsonRequestBehavior.AllowGet);
         }
-
         public ActionResult Index()
         {
             return View(new List<Pepople>
