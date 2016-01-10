0e37bac7b6384bb5f3ed347ce919463595506e3b
happyu0223 <753556090@qq.com>
2016-01-10 18:07:15 +0800

    ff

--- KnockOutDemoMvc/KnockOutDemoMvc/Scripts/tinymce/plugins/codemirror/langs/zh_CN.js
+++
@@ -1,8 +0,0 @@
-﻿tinymce.addI18n('zh_CN',{
-	'HTML source code': 'HTML 源代码',
-	'Start search': '开始搜索',
-	'Find next': '查找下一个',
-	'Find previous': '查找上一个',
-	'Replace': '替换',
-	'Replace all': '替换全部'
-});
